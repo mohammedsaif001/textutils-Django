ea239b9af3dbe35d49bc6226844d6ef8d24899d4
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-11 21:57:48 +0530

    3 Using Templates

--- textutils/views.py
+++ textutils/views.py
@@ -1,9 +1,12 @@
 #I have created this File - mohammedsaif001
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>Home</h1>")
+    params = {'name':"saif",'learning':'Django'}
+    return render(request,'index.html',params)
+    # return HttpResponse("<h1>Home</h1>")
 
 def removePunc (request):
     return HttpResponse('Remove Punctuation')
