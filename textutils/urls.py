3d9f4b50c0d054b7dde1d39d8753eb43119f0570
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-13 12:23:04 +0530

    5 Adding Backend

--- textutils/urls.py
+++ textutils/urls.py
@@ -21,10 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
-    path('removepunc', views.removePunc,name='removePunc'),
-    path('capitalize', views.capitalize,name='capitalize'),
-    path('newlineremove', views.newlineremove,name='newlineremove'),
-    path('spaceremove', views.spaceremove,name='spaceremove'),
-    path('charcount', views.charcount,name='charcount')
+    path('analyze', views.analyze,name='analyze')
     
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -1,29 +1,31 @@
 #I have created this File - mohammedsaif001
 
+from string import punctuation
+from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
-    # return HttpResponse("<h1>Home</h1>")
 
-def removePunc (request):
-    # Get Text from the User from webPage
+def analyze (request):
     djText = request.GET.get('text','default')
     print(djText)
-    return HttpResponse('Remove Punctuation')
-
-def capitalize (request):
-    return HttpResponse('Capitalize First Letter')
-
-def newlineremove (request):
-    return HttpResponse('newlineremove')
-
-def spaceremove (request):
-    return HttpResponse('space remove')
-
-def charcount (request):
-    return HttpResponse('charcount')
-
-
-
+    
+    removep = request.GET.get('removepunc','off')
+    print(removep)
+    
+    punctuations = ''' 
+    '~`!@#$%^&*()-_=+\|}{":?><,./;'[]\}
+    '''
+    analyzed = ""
+    
+    if removep == "on":
+        for char in djText:
+            if char not in punctuations:
+                analyzed=analyzed+char
+        
+        params = {'purpose': 'Removed Punctuation','analyzedText' : analyzed}
+        return render(request,'analyze.html',params)
+    else:
+        return HttpResponse("Error")
